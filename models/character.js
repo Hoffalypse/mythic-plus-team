3642ef1446b4f7e2ff9c5e53d3946463618904db
Bryan Wienhoff <106857564+Hoffalypse@users.noreply.github.com>
2022-09-27 01:53:23 -0600

    added main handlebar and all deps

---
+++ models/character.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+class Character extends Model { }
+
+
+Character.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+       
+      },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+     
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
