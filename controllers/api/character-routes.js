b778c8a658a57add992095f9c0880717b0a223d0
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-09-28 17:00:49 -0600

    fixing odd merge

--- controllers/api/character-routes.js
+++ controllers/api/character-routes.js
@@ -26,28 +26,23 @@ router.post('/', async (req, res) => {
     res.status(420).json(err);
   }
 });
-<<<<<<< HEAD
-router.post('/',  async (req, res) => {
-    try {
-   
-      const newCharacter = await Character.create({
-        name: req.body.name,
-        role: req.body.role,
-        avatar: req.body.avatar,
-        char_class: req.body.char_class,
-        spec: req.body.spec,
-        ilvl: req.body.ilvl,
-        current_m_score: req.body.current_m_score,
-        user_id: req.session.user_id,
-        
-      });
-        console.log("made it coach");
-      res.status(200).json(newCharacter);
-    } catch (err) {
-      res.status(420).json(err);
-    }
-  });
-=======
->>>>>>> b6210d821799ae70c8ed22236b08bc2149a7d1b4
+router.post('/', async (req, res) => {
+  try {
+    const newCharacter = await Character.create({
+      name: req.body.name,
+      role: req.body.role,
+      avatar: req.body.avatar,
+      char_class: req.body.char_class,
+      spec: req.body.spec,
+      ilvl: req.body.ilvl,
+      current_m_score: req.body.current_m_score,
+      user_id: req.session.user_id,
+    });
+    console.log('made it coach');
+    res.status(200).json(newCharacter);
+  } catch (err) {
+    res.status(420).json(err);
+  }
+});
 
 module.exports = router;
