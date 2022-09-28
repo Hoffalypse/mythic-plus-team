eba9975a8ff3524ca469767730d4f713a8c5eb32
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-09-28 12:56:07 -0600

    added to the char model

--- models/character.js
+++ models/character.js
@@ -1,9 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-
 class Character extends Model {}
-
 
 Character.init(
   {
@@ -14,16 +12,49 @@ Character.init(
       autoIncrement: true,
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    avatar: {
+      type: DataTypes.BLOB,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role_icon: {
+      type: DataTypes.BLOB,
+      allowNull: false,
+    },
+    class: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    spec: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ilvl: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    current_m_score: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    note: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
     user_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'user',
         key: 'id',
       },
-  },
+    },
   },
   {
     sequelize,
