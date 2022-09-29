17b05934a925784155143005bcd2fef0f9e687de
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-09-29 13:53:41 -0600

    added delete button and routes for teammates

--- controllers/api/teammate-routes.js
+++ controllers/api/teammate-routes.js
@@ -1,35 +1,54 @@
 const router = require('express').Router();
 const { User, Character, Teammates } = require('../../models');
 const withAuth = require('../../utils/auth');
+const { capitalizeFirstLetter } = require('../../utils/helpers');
 
 //---------------- at api/teammates------------------
 
 router.get('/', (req, res) => {
-    Teammates.findAll({    
-    })
-      .then((dbUserData) => res.json(dbUserData))
-      .catch((err) => {
-        res.status(500).json(err);
-      });
-  });
-  router.post('/', async (req, res) => {
-    try {
-      const newTeammate = await Teammates.create({
-        name: req.body.name,
-        role: req.body.role,
-        avatar: req.body.avatar,
-        char_class: req.body.char_class,
-        spec: req.body.spec,
-        ilvl: req.body.ilvl,
-        current_m_score: req.body.current_m_score,
-        region: req.body.region,
-        realm: req.body.realm,
-        character_id: req.body.character_id,
-      });
-      
-      res.status(200).json(newTeammate);
-    } catch (err) {
-      res.status(420).json(err);
+  Teammates.findAll({})
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+router.post('/', async (req, res) => {
+  try {
+    const newTeammate = await Teammates.create({
+      name: capitalizeFirstLetter(req.body.name),
+      role: req.body.role,
+      avatar: req.body.avatar,
+      char_class: req.body.char_class,
+      spec: req.body.spec,
+      ilvl: req.body.ilvl,
+      current_m_score: req.body.current_m_score,
+      region: req.body.region,
+      realm: req.body.realm,
+      character_id: req.body.character_id,
+    });
+
+    res.status(200).json(newTeammate);
+  } catch (err) {
+    res.status(420).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const data = await Teammates.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!data) {
+      res.status(400).json({ message: 'No teammate with this id!' });
+      return;
     }
-  });
-  module.exports = router;
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
