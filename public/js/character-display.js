a5208d0ffbcdd644e8c99f127510fe1aaa1c61ec
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-10-02 08:38:12 -0600

    refactored displaycharacterhandler to async await

--- public/js/character-display.js
+++ public/js/character-display.js
@@ -1,17 +1,18 @@
-const displayCharacterHandler = (event) => {
+const displayCharacterHandler = async (event) => {
   event.preventDefault();
   const id = event.target.attributes['data-id'].nodeValue;
-  fetch(`/api/characters/${id}`, {
-    method: 'PUT',
-    body: JSON.stringify({
-      id,
-    }),
-    headers: { 'Content-Type': 'application/json' },
-  })
-    .then(function () {
-      document.location.reload();
-    })
-    .catch((err) => console.log(err));
+  try {
+    const res = await fetch(`/api/characters/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        id,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    document.location.reload();
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 charArr = document.querySelectorAll('#char-pick');
