75cb349b9765271d5cdb7693fa854b7efc659ffd
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-10-04 10:01:55 -0600

    fixed minor error with api call by adding a second try catch

--- controllers/api/character-routes.js
+++ controllers/api/character-routes.js
@@ -5,6 +5,7 @@ const withAuth = require('../../utils/auth');
 const { capitalizeFirstLetter } = require('../../utils/helpers');
 const { options } = require('./user-routes');
 const axios = require('axios').default;
+const slug = require('slug');
 
 //--------------at api/characters--------------------------
 
@@ -31,64 +32,66 @@ router.get('/:id', withAuth, async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    
-    
-    async function makeRequest(){
-      const response = await axios.get(
-        `https://raider.io/api/v1/characters/profile?region=${req.body.region}&realm=${req.body.realm}&name=${req.body.name}&fields=gear%2Cguild%2Cmythic_plus_scores_by_season%3Acurrent`
-      );
-      var options = {
+    async function makeRequest() {
+      try {
+        const response = await axios.get(
+          `https://raider.io/api/v1/characters/profile?region=${req.body.region}&realm=${req.body.realm}&name=${req.body.name}&fields=gear%2Cguild%2Cmythic_plus_scores_by_season%3Acurrent`
+        );
+        var options = {
           method: 'GET',
-          url: `https://us.api.blizzard.com/profile/wow/character/${req.body.realm}/${req.body.name}/character-media`,
+          url: `https://us.api.blizzard.com/profile/wow/character/${slug(
+            req.body.realm
+          )}/${req.body.name}/character-media`,
           params: {
             namespace: `profile-${req.body.region}`,
             locale: `en_${req.body.region}`,
             access_token: `${req.session.token.access_token}`,
-        },
-         
+          },
+        };
+        var newRes = await axios(options);
+        console.log(newRes.data.assets[3].value);
+
+        const newCharacter = await Character.create({
+          name: capitalizeFirstLetter(req.body.name),
+          role: response.data.active_spec_role,
+          avatar: response.data.thumbnail_url,
+          image: newRes.data.assets[3].value,
+          char_class: response.data.class,
+          spec: response.data.active_spec_name,
+          ilvl: response.data.gear.item_level_equipped,
+          current_m_score:
+            response.data.mythic_plus_scores_by_season[0].scores.all,
+          region: req.body.region,
+          realm: req.body.realm,
+          user_id: req.session.user_id,
+        });
+
+        const char = await Character.findOne({
+          where: { name: `${req.body.name}` },
+        });
+        const newTeammate = await Teammates.create({
+          name: capitalizeFirstLetter(req.body.name),
+          role: response.data.active_spec_role,
+          avatar: response.data.thumbnail_url,
+          char_class: response.data.class,
+          spec: response.data.active_spec_name,
+          ilvl: response.data.gear.item_level_equipped,
+          current_m_score:
+            response.data.mythic_plus_scores_by_season[0].scores.all,
+          region: req.body.region,
+          realm: req.body.realm,
+          character_id: char.id,
+        });
+        res.status(200).json({ newCharacter, newTeammate });
+      } catch (err) {
+        console.log(err);
       }
-      var newRes = await axios(options)
-      console.log(newRes.data.assets[3].value) 
-
-    const newCharacter = await Character.create({
-      name: capitalizeFirstLetter(req.body.name),
-      role: response.data.active_spec_role,
-      avatar: response.data.thumbnail_url,
-      image: newRes.data.assets[3].value,
-      char_class: response.data.class,
-      spec: response.data.active_spec_name,
-      ilvl: response.data.gear.item_level_equipped,
-      current_m_score: response.data.mythic_plus_scores_by_season[0].scores.all,
-      region: req.body.region,
-      realm: req.body.realm,
-      user_id: req.session.user_id,
-    });
- 
-    
-
-    const char = await Character.findOne({
-      where: { name: `${req.body.name}` },
-    });
-    const newTeammate = await Teammates.create({
-      name: capitalizeFirstLetter(req.body.name),
-      role: response.data.active_spec_role,
-      avatar: response.data.thumbnail_url,
-      char_class: response.data.class,
-      spec: response.data.active_spec_name,
-      ilvl: response.data.gear.item_level_equipped,
-      current_m_score: response.data.mythic_plus_scores_by_season[0].scores.all,
-      region: req.body.region,
-      realm: req.body.realm,
-      character_id: char.id,
-    });
-  
-    res.status(200).json({ newCharacter, newTeammate });
-  }
-  makeRequest();
+    }
+    makeRequest();
   } catch (err) {
     res.status(420).json(err);
   }
-})
+});
 
 router.put('/:id', withAuth, async (req, res) => {
   try {
