524e7dd7d0c59ba5f974e48b9f80d577c33372a5
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-10-04 14:32:28 -0600

    Teammate page styling made m+ score round to nearest int

--- public/js/totals.js
+++ public/js/totals.js
@@ -1,20 +1,14 @@
+function mScoreAverage(params) {
+  numberArr = document.querySelectorAll('.numbers');
 
-function mScoreAverage  (params) {
-    numberArr = document.querySelectorAll(".numbers");
-
-   let total = 0;
-   for (let index = 0; index < numberArr.length; index++) {
+  let total = 0;
+  for (let index = 0; index < numberArr.length; index++) {
     let score = numberArr[index].outerText;
-    total = Number(score) + total 
-   }
-   let average_m_score = total/numberArr.length
-    
-    document.getElementById("average-m").innerHTML = average_m_score;
+    total = Number(score) + total;
+  }
+  let average_m_score = Math.round(total / numberArr.length);
+
+  document.getElementById('average-m').innerHTML = average_m_score;
 }
 
 mScoreAverage();
-
-
- 
-
-
