c068f2331dd5f088a27160763c4638bcff4ae8aa
levimendyk <levi3743@gmail.com>
2022-10-03 19:05:10 -0600

    messing with email input form

--- public/js/email-support.js
+++ public/js/email-support.js
@@ -1,18 +1,18 @@
-const submitEl = document.querySelector('#submit');
-const nameInput = document.querySelector('#firstName');
-const emailInput = document.querySelector('#email');
-const submissionResponseEl = document.querySelector('#response');
+const submitEl = document.querySelector("#submit");
+const nameInput = document.querySelector("#firstName");
+const emailInput = document.querySelector("#email");
+const submissionResponseEl = document.querySelector("#response");
 
 function showResponse(event) {
   event.preventDefault();
   console.log(event);
   let response =
-    'Thank you for your submission ' +
+    "Thank you for your submission " +
     nameInput.value +
-    '! We will reach out to you at ' +
+    "! We will reach out to you at " +
     emailInput.value +
-    '.';
+    ".";
   submissionResponseEl.textContent = response;
 }
 
-submitEl.addEventListener('click', showResponse);
+submitEl.addEventListener("click", showResponse);
